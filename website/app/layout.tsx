732e5af8ff39cb3fa45dbb9df69fe9e76f4a22c9
Gwi7d31 <ggwiazda@gmail.com>
2025-06-07 14:29:39 -0400

    Stopping for the afternoon with setup.

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -24,12 +24,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode;}>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
-        <Header />
-        {children}
-        <Footer />
+          <Header />
+          {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  content: [/*...*/],
-  plugins: [
-    require("tailwindcss-animate"),
-  ],
+    darkMode: 'class',
+    content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: { extend: {} },
+    plugins: [
+        require("tailwindcss-animate"),
+    ],
 };
