bf37ce7d70c65ec7d8f371bbe1ce366ae75ef536
Gwi7d82 <ggwiazda@gmail.com>
2025-06-07 08:16:15 -0400

    Very basic header/footer template. Need to setup auto-refresh in ci. Not happening yet.

---
+++ website/app/template/footer.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+
+export default function Footer() {
+
+    return (
+        <footer>
+            <h3>Footer</h3>
+        </footer>
+    )
+}
---
+++ website/app/template/header.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+
+export default function Header() {
+
+    return (
+        <header>
+            <h3>Header</h3>
+        </header>
+    )
+}
